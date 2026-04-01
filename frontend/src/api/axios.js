3b854fd16334bf9192f625e2bc5789c207cae39f
Priyam-0001 <priyamnath5@gmail.com>
2026-04-02 00:59:39 +0530

    added base url in env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+console.log(apiUrl)
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: `${apiUrl}/api`
 });
 
 // Attach token automatically
